e532e185b79c01a5b7ef346d4262dd2f67234a7b
SangNL7484 <sangnl7484@co-well.vn>
2023-01-10 13:22:58 +0700

    update

--- resources/views/blocks/story.blade.php
+++ resources/views/blocks/story.blade.php
@@ -2,7 +2,6 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
-                <!-- <span>We Love Each Other</span> -->
                 <h2><a href="/chuyen-tinh">Chuyện Tình</a></h2>
                 <p class="poem-love">
                     Tâm bất biến giữa dòng đời vạn biến</br>
@@ -13,7 +12,7 @@
         <div class="row">
             <div class="col-md-12 col-md-offset-0">
                 <ul class="timeline animate-box">
-		    <li class="animate-box">
+		            <li class="animate-box">
                         <div class="timeline-badge lazy" data-src="images/story/ky-yeu-cap-3.jpg"></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
@@ -21,7 +20,7 @@
                                 <span class="date">05/08/2012</span>
                             </div>
                             <div class="timeline-body">
-                                <p>Tôi từng nghe nói rằng "Cô gái đi bên bạn năm 17 tuổi sẽ không cùng bạn đi hết chặng đường đời". Vì thế tôi đã đưa ra một quyết định vô cùng đột phá và táo bạo, tránh cái dớp 17.<br/>Nói yêu cô ấy năm 15 tuổi.</p>
+                                <p>Mình từng nghe nói "Cô gái đi bên bạn năm 17 tuổi sẽ không cùng bạn đi hết chặng đường đời". Vì thế mình đã đưa ra một quyết định vô cùng đột phá và táo bạo để tránh cái dớp 17.<br/>Nói yêu cô ấy năm 15 tuổi.</p>
                             </div>
                         </div>
                     </li>
@@ -45,7 +44,7 @@
                                 <span class="date">12/09/2014</span>
                             </div>
                             <div class="timeline-body">
-                                <p>Đón đưa nhau đi học hàng ngày qua con dốc dài, con dốc chất chứa những khoảnh khắc êm đềm của cả một hành trình tình yêu của hai đứa.</p>
+                                <p>Đón đưa nhau đi học hàng ngày qua con dốc dài, con dốc chất chứa những khoảnh khắc êm đềm của cả một hành trình tình yêu đôi đứa.</p>
                             </div>
                         </div>
                     </li>
@@ -69,7 +68,7 @@
                                 <span class="date">05/09/2015</span>
                             </div>
                             <div class="timeline-body">
-                                <p>Những ngày đầu bỡ ngỡ khi mới lên Hà Nội. Hai đứa đầy hứng khởi đi thăm thú khắp nơi, đưa nhau qua những con phố lớn nhỏ.</p>
+                                <p>Những ngày đầu bỡ ngỡ khi mới lên Hà Nội. Hai đứa đầy hứng khởi đi thăm thú nhiều nơi, đưa nhau qua khắp những con phố lớn nhỏ.</p>
                             </div>
                         </div>
                     </li>
@@ -93,11 +92,11 @@
                                 <span class="date">09/04/2016</span>
                             </div>
                             <div class="timeline-body">
-                                <p>Chuyến đi thăm nuôi "cô dân quân" hơi nhiều sóng gió :v Nhờ đó mà mình nhận ra có thể vượt qua mọi khó khăn để được ở gần nhau.</p>
+                                <p>Chuyến đi thăm nuôi "cô dân quân" hơi nhiều sóng gió và bão táp :v Nhờ đó mà mình nhận ra có thể vượt qua mọi khó khăn để được ở gần nhau.</p>
                             </div>
                         </div>
                     </li>
-		    <li class="timeline-inverted animate-box">
+		            <li class="timeline-inverted animate-box">
                         <div class="timeline-badge lazy" data-src="images/story/ho-tay.jpg"></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
@@ -105,7 +104,7 @@
                                 <span class="date">11/06/2017</span>
                             </div>
                             <div class="timeline-body">
-                                <p>Hồ Tây là địa điểm lượn lờ thường xuyên của 2 đứa, cùng nhau lượn hết vòng hồ tây, cùng kể cho nhau nghe những điều thú vị trong cuộc sống.</p>
+                                <p>Hồ Tây là địa điểm lượn lờ thường xuyên của 2 đứa, chở nhau đi hết vòng hồ, cùng kể cho nhau nghe những điều thú vị trong cuộc sống.</p>
                             </div>
                         </div>
                     </li>
@@ -129,7 +128,7 @@
                                 <span class="date">28/04/2019</span>
                             </div>
                             <div class="timeline-body">
-                                <p>Một lần nữa đồng hành cùng nhau trong cột mốc tiếp theo của cuộc đời</p>
+                                <p>Một lần nữa đồng hành cùng nhau trong cột mốc tiếp theo của cuộc đời. Bước qua ngưỡng cửa Đại học để đến với bao bộn bề lo toan cuộc sống.</p>
                             </div>
                         </div>
                     </li>
@@ -137,15 +136,15 @@
                         <div class="timeline-badge lazy" data-src="images/story/du-lich-ha-long.jpg"></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
-                                <h3 class="timeline-title">Hạ Long cùng nhóm bạn thân</h3>
+                                <h3 class="timeline-title">Hạ Long cùng hội bạn thân</h3>
                                 <span class="date">18/07/2020</span>
                             </div>
                             <div class="timeline-body">
-                                <p>Cùng với hội bạn thân đi du lịch. Chuyến đi hơi ngắn ngày nhưng chúng ta đã có những kỷ niệm tuyệt vời và khó quên bên cạnh bạn bè.</p>
+                                <p>Đi du lịch với hội bạn thân. Chuyến đi hơi ngắn ngày nhưng chúng ta đã có những kỷ niệm tuyệt vời và khó quên bên cạnh bạn bè.</p>
                             </div>
                         </div>
                     </li>
-		    <li class="timeline-inverted animate-box">
+                    <li class="timeline-inverted animate-box">
                         <div class="timeline-badge lazy" data-src="images/story/sinh-nhat.jpg"></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
@@ -153,11 +152,11 @@
                                 <span class="date">26/10/2021</span>
                             </div>
                             <div class="timeline-body">
-                                <p>Cùng nhau trưởng thành, trải qua ngày sinh nhật đặc biệt và khó quên.</p>
+                                <p>Cùng nhau trưởng thành qua từng năm tháng, những ngày sinh nhật đặc biệt và khó quên.</p>
                             </div>
                         </div>
                     </li>
-		    <li class="animate-box">
+                    <li class="animate-box">
                         <div class="timeline-badge lazy" data-src="images/story/hen-ho.jpg"></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
@@ -165,7 +164,7 @@
                                 <span class="date">10/04/2022</span>
                             </div>
                             <div class="timeline-body">
-                                <p>Sau những guồng xoáy của công việc, vẫn dành cho nhau thời gian vào những buổi hẹn hò đơn giản mà hạnh phúc.</p>
+                                <p>Sau những guồng quay của công việc, vẫn dành thời gian cho nhau vào những buổi hẹn hò êm đềm mà hạnh phúc.</p>
                             </div>
                         </div>
                     </li>
@@ -173,11 +172,11 @@
                         <div class="timeline-badge lazy" data-src="images/story/du-lich-da-nang.jpg"></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
-                                <h3 class="timeline-title">Du lịch Huế - Đà Nẵng - Hội An</h3>
+                                <h3 class="timeline-title">Tour Huế - Đà Nẵng - Hội An</h3>
                                 <span class="date">16/07/2022</span>
                             </div>
                             <div class="timeline-body">
-                                <p>Chuyến bay đầu tiên cùng nhau của 2 đứa. Cách ngày bay một tuần cả 2 đều bị Covid nhưng thật may đã kịp khỏi và chúng ta đã có một chuyến đi trọn vẹn, nhẹ nhàng và đầy ý nghĩa.</p>
+                                <p>Chuyến bay đầu tiên cùng nhau của 2 đứa. Cách ngày bay một tuần cả 2 đều bị Covid nhưng thật may đã kịp khỏi để chúng ta đã có một chuyến đi trọn vẹn, nhẹ nhàng và đầy ý nghĩa.</p>
                             </div>
                         </div>
                     </li>
