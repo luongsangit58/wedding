62a7e9da5d739700335436179dd5b4cd0f60c14c
SangNL7484 <sangnl7484@co-well.vn>
2023-01-12 14:00:39 +0700

    update code

--- app/Http/Controllers/WishesController.php
+++ app/Http/Controllers/WishesController.php
@@ -221,4 +221,22 @@ class WishesController extends Controller
     
         return substr($name, 0, $len) . str_repeat('*', $len) . "@" . end($em);   
     }
+
+    public function getRealIPAddress()
+    {
+        if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+            //check ip from share internet
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        }else if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+            //to check ip is pass from proxy
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }else{
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+
+        return [
+            'error' => 0,
+            'ip' => $ip
+        ];
+    }
 }
