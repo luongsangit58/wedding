10ae4998adad523c127360003dd1d25048e56848
sangnl <sangnl@viettel.com.vn>
2024-05-23 16:29:56 +0700

    update cta

--- resources/views/blocks/header.blade.php
+++ resources/views/blocks/header.blade.php
@@ -1,11 +1,11 @@
 <header id="fh5co-header" class="fh5co-cover">
-    <div class="overlay overlay-pc-{{ rand(1, 2) }} overlay-mb-{{ rand(1, 2) }}"></div>
+    <div class="overlay overlay-pc-{{ rand(1, 3) }} overlay-mb-{{ rand(1, 4) }}"></div>
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2 text-center box-overley">
                 <div class="display-t">
                     <div class="display-tc">
-                        <h1 class="title-header">Sang &amp; Trang</h1>
+                        <h1 class="title-header">Sang Trang</h1>
                         <p class="poem-love poem-love-shadow" style="color: #fff;">
                             Mình cùng bước khi hoàng hôn nắng tắt<br/>
                             Rạng tỏ bình minh vẫn siết chặt bàn tay
